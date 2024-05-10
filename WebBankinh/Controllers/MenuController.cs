090fa0f2f11567d61fbd784480bc74b7e11c6d75
Trần Ngọc Thanh <56815987+thq2776@users.noreply.github.com>
2024-05-10 17:31:21 +0700

    Update
    Update Controller

---
+++ WebBankinh/Controllers/MenuController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBankinh.Models;
+using WebBankinh.Models.Dtos;
+using WebBankinh.Models.EF;
+
+namespace WebBankinh.Controllers
+{
+    public class MenuController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: Menu
+        public ActionResult Index()
+        {
+            ViewBag.Banner2 = db.CauHinhs.Where(x => x.Id == 1).FirstOrDefault().Banner2;
+            return View();
+        }
+        public ActionResult MenuLoaiSanPham()
+        {
+            var listProduct = new List<ProductMapping>();
+            var items = db.LoaiSanPhams.OrderByDescending(x => x.Id).Where(x => x.Active == false || x.Active == null).ToList();
+
+            foreach (var item in items)
+            {
+                var product = db.BienTheSanPhams.Where(f => f.SanPham.IdLoaiSanPham == item.Id).OrderByDescending(x => x.Id).ToList();
+                listProduct.Add(new ProductMapping() { LoaiSanPham = item, BienTheSanPhams = product});
+            }
+            return PartialView("MenuLoaiSanPham", listProduct);
+        }
+
+        public ActionResult ChiTietSP(int id)
+        {
+            var item = db.BienTheSanPhams.Include("HinhAnh").Include("SanPham").FirstOrDefault(f => f.SanPham.Id == id);
+            ViewBag.CategoryName = db.LoaiSanPhams.FirstOrDefault(f => f.Id == item.SanPham.IdLoaiSanPham).TenLoai;
+            return View(item);
+        }
+    }
+}
+
---
+++ WebBankinh/Controllers/SanPhamController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebBankinh.Models;
+
+namespace WebBankinh.Controllers
+{
+    public class SanPhamController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        // GET: SanPham
+        public ActionResult Index(/*int? page*/)
+        {
+           /* int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            var products = db.SanPhams.OrderBy(p => p.TenSanPham);
+            return View(products.ToPagedList(pageNumber, pageSize));*/
+            return View();
+        }
+    }
+}
